96c91f9ed48d4ed56d117e6891c9e1ea77fed789
spences10 <spences10apps@gmail.com>
2023-07-08 17:23:18 +0100

    refactor: :recycle: use upstash redis

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,6 +1,24 @@
-import { REDIS_CONNECTION } from '$env/static/private'
-import Redis from 'ioredis'
+import { building } from '$app/environment'
+import {
+	UPSTASH_REDIS_REST_TOKEN,
+	UPSTASH_REDIS_REST_URL,
+} from '$env/static/private'
+import { Ratelimit } from '@upstash/ratelimit'
+import { Redis } from '@upstash/redis'
 
-export default REDIS_CONNECTION
-	? new Redis(REDIS_CONNECTION)
-	: new Redis()
+let redis: Redis
+let ratelimit: Ratelimit
+
+if (!building) {
+	redis = new Redis({
+		url: UPSTASH_REDIS_REST_URL,
+		token: UPSTASH_REDIS_REST_TOKEN,
+	})
+
+	ratelimit = new Ratelimit({
+		redis,
+		limiter: Ratelimit.slidingWindow(10, '30 s'),
+	})
+}
+
+export { redis, ratelimit }
