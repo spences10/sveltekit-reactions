7affb7bbfe6ee87ba4d78bea42be3ace5d103feb
spences10 <spences10apps@gmail.com>
2023-07-15 12:02:20 +0100

    feat: :sparkles: add number flip to main page

--- src/lib/components/reactions.svelte
+++ src/lib/components/reactions.svelte
@@ -3,6 +3,7 @@
 	import { reactions } from '$lib/config'
 	import type { ActionResult } from '@sveltejs/kit'
 	import { writable } from 'svelte/store'
+	import NumberFlip from './number-flip.svelte'
 
 	export let path: string | null = '/'
 	export let data: ReactionsData | null = null
@@ -17,7 +18,20 @@
 			}, result?.data?.time_remaining * 1000)
 		}
 	}
+
+	let show_static_buttons = false
 </script>
+
+<div class="flex justify-center mb-4">
+	<label class="label cursor-pointer space-x-5">
+		<span class="label-text">Show static buttons</span>
+		<input
+			type="checkbox"
+			bind:checked={show_static_buttons}
+			class="toggle toggle-primary toggle-sm"
+		/>
+	</label>
+</div>
 
 <div class="flex justify-center">
 	<form
@@ -32,19 +46,30 @@
 		}}
 		class="grid grid-cols-2 gap-5 sm:flex"
 	>
-		{#each reactions as reaction}
-			<button
-				name="reaction"
-				type="submit"
-				value={reaction.type}
-				class="btn btn-primary shadow-xl text-3xl font-bold"
-				disabled={$button_disabled}
-			>
-				<span>
-					{reaction.emoji}
-					{data?.counts?.[reaction.type] || 0}
-				</span>
-			</button>
-		{/each}
+		{#if !show_static_buttons}
+			{#each reactions as reaction}
+				<NumberFlip
+					count={data?.counts?.[reaction.type] || 0}
+					emoji={reaction.emoji}
+					value={reaction.type}
+					disabled={$button_disabled}
+				/>
+			{/each}
+		{:else}
+			{#each reactions as reaction}
+				<button
+					name="reaction"
+					type="submit"
+					value={reaction.type}
+					class="btn btn-primary shadow-xl text-3xl font-bold"
+					disabled={$button_disabled}
+				>
+					<span>
+						{reaction.emoji}
+						{data?.counts?.[reaction.type] || 0}
+					</span>
+				</button>
+			{/each}
+		{/if}
 	</form>
 </div>
