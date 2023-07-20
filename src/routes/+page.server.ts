676a9fe2282e44838fa33a643c61a658c65543c5
spences10 <spences10apps@gmail.com>
2023-07-20 16:22:21 +0100

    refactor: :recycle: remove auto import js extensions

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,4 +1,4 @@
-import { get_reaction_count } from '$lib/utils.js'
+import { get_reaction_count } from '$lib/utils'
 
 export const load = async ({ url: { pathname } }) => {
 	const count = await get_reaction_count(pathname)
